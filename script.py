24f23574685aa5004f41d58caa2816730d3156fb
Billy Tindall <billytindall98@yahoo.com>
2020-11-07 23:20:17 +0000

    inport modules and packages

---
+++ script.py
@@ -0,0 +1,9 @@
+# Foreign exchange rate API tool
+
+# Import required Modules / Packages
+import json
+import requests
+import pandas as pd
+import matplotlib.pyplot as plt
+import numpy as np
+import datetime
