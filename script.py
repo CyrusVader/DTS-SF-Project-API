74c5d6c6e410da4b4b67d42c85516d305f50dba3
Billy Tindall <billytindall98@yahoo.com>
2020-11-07 23:45:19 +0000

    added function to plot line plot and added function call to run the script

--- script.py
+++ script.py
@@ -32,7 +32,29 @@ def format_historic_data():
 	print(hist_data)
 	return hist_data
 
-	
+
+def plot_historic_data():
+	formated_data = format_historic_data()
+	# Set axis of plot from different data
+	df = pd.DataFrame(formated_data)
+	x = df['date']
+	y1 = df['primary_rate']
+	y2 = df['secondary_rate']
+	fig = plt.figure(figsize=(15,6))
+	plt.xticks(rotation=90)
+	# Set labels of plot
+	plt.xlabel('Date')
+	plt.ylabel('Exchange Rate to Euro')
+	# Plot data
+	fig.suptitle(primary_currency + " compared with " + secondary_currency + " base EUR Rate", fontsize=16)
+	plt.xticks(rotation=90)
+	plt.xticks(np.arange(0, len(formated_data), 5))
+	plt.plot(x, y1, "-b", label=primary_currency)
+	plt.plot(x, y2, "-r", label=secondary_currency)
+	plt.legend(loc="upper left")
+	plt.show()
+
+
 # requesting input from the user on details of request
 start_date = validate(input("What start date would you like to look at? (Date format: YYYY-MM-DD):"))
 print("Choosen start date: " + start_date)
@@ -44,3 +66,7 @@ secondary_currency = input("What is the secondary currency you would like to com
 print("Choosen secondary currency: " + secondary_currency)
 # Concatenate request URL from user input data
 request_url = historical_url + "?start_at=" + str(start_date) + "&end_at=" + str(end_date) + "&symbols=" + primary_currency.upper() + "," + secondary_currency.upper()
+
+
+# Load function
+plot_historic_data()
